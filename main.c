ebe16ab79b887ef966981f3095bd1014b2e57376
hackerzinho-senai <158463016+hackerzinho-senai@users.noreply.github.com>
2024-03-05 11:56:26 +0000

    bom dia e calculadora

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
+// #include <stdio.h>
+
+// void dizerBoatarde(char nome[20]){
+//   printf("Boa Tarde, %s gostoso", nome);
+// }
+
+// int main(void) {
+//   char nome1[20];
+//   printf("Qual seu nome?\nNome: ");
+//   scanf("%s", nome1);
+//   dizerBoatarde(nome1);
+//   return 0;
+// }
+
+//Só Faz a soma de dois números
 #include <stdio.h>
+  float soma(float a, float b) {
+    return a + b;
+  }
 
-void dizerBoatarde(char nome[20]){
-  printf("Boa Tarde, %s gostoso", nome);
-}
-
-int main(void) {
-  char nome1[20];
-  printf("Qual seu nome?\nNome: ");
-  scanf("%s", nome1);
-  dizerBoatarde(nome1);
-  return 0;
+  
+int main(void){
+  int a, b, resu;
+  printf("Digite um número: ");
+  scanf("%f", &a);
+  printf("Digite outro número: ");
 }
